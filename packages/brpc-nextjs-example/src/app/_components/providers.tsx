9e47113eb4f1338cb4123049951d3a6d48772aae
Abgier Avraha <abgier.avraha@gmail.com>
2025-08-03 20:35:30 +1000

    ideas for built in tanstack integration

--- packages/brpc-nextjs-example/src/app/_components/providers.tsx
+++ packages/brpc-nextjs-example/src/app/_components/providers.tsx
@@ -10,6 +10,7 @@ import {
 	hydrate,
 	QueryClient,
 	QueryClientProvider,
+	useQueryClient,
 } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -26,6 +27,31 @@ export function Providers(props: {
 	}, []);
 
 	hydrate(queryClient, props.state);
+
+	/* TODO:
+		1. Every rpc can have a type param specifying either mutation or query
+		2. Change BrpcProvider to BrpcReactProvider
+			Get the query client with `useQueryClient`
+			Wrap the brpc client with a brpcQueryClient which will create another Proxy
+			That Proxy will leverage the query client and return the api channel with extra functions
+			{
+				useSuspenseQuery
+				useQuery
+				exec
+			}
+				OR 
+			{
+				useMutation
+				exec
+			}
+			The query keys should auto populate and the only thing you need to worry about
+			is passing the args
+			ex: api.echo.useSuspenseQuery({phrase: "test"})
+		3. Do a similar with proxies to provide `prefetchQuery` on the server
+			The query keys should auto populate and the only thing you need to worry about
+			is passing the args
+			ex: await api.echo.prefetchQuery({phrase: "test"})
+	*/
 
 	return (
 		<QueryClientProvider client={queryClient}>
